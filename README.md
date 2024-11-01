266b49d02ab5965c2799789ef1116a13c694072a
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2024-10-31 18:00:36 -0700

    initial project setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # halloween_movie_rec
+
+Movie recommender halloween themed
---
+++ halloween/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='bg-orange-700 w-full h-screen'>
+      <h1 className='text-white text-4xl w-full text-center p-4'>Halloween Movie Recommender</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
